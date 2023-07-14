ef471c782a9a2a6cffad71881e1c5866b80cabd9
valeria <valeria@MacBook-Air-yuriy.local>
2023-07-14 15:05:50 +0300

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// ---1
+
+let result = 5+5+"5";
+console.log(result);
+console.log(typeof result);
+
+// ---2
+
+let email = "lera.protcyshina@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// ---3
+
+let part1 = "My";
+let part2 = "name";
+let part3 = "is";
+let fullName = `${part1} ${part2} ${part3} Viktor`;
+console.log(fullName);
+
+// ---4
+
+let userName = "Valeria";
+let payment = 100;
+
+alert(`Thank you, ${userName}. You have to pay ${payment}$`);
